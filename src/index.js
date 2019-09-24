f195fb4869622f013c90ecfa9ed586b523677f6f
msv1990 <htconemailbox@gmail.com>
2019-09-24 15:21:00 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@ function eval() {
     return;
 }
 
+
 function expressionCalculator(expr) {
-    // write your solution here
+    const bracketsOpen = expr.split('').filter(el => el === '(');
+    const bracketsClose = expr.split('').filter(el => el === ')');
+
+    if(bracketsOpen.length !== bracketsClose.length){
+        throw new Error ('ExpressionError: Brackets must be paired');
+    }
+
+    let output = (new Function('return '  + expr))();
+
+    if(output === Infinity) {
+        throw new Error ('TypeError: Devision by zero.');
+    }
+
+    return output;
 }
 
 module.exports = {
